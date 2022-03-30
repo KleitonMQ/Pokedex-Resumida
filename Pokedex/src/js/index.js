09dca10429f98ccc16c791eeeb9d55396583259a
Kleiton MQ <87434618+KleitonMQ@users.noreply.github.com>
2022-03-30 15:37:43 -0300

    Add files via upload

---
+++ Pokedex/src/js/index.js
@@ -0,0 +1,20 @@
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const cartaoPokemonParaAbrir = document.getElementById('cartao-'+ idPokemonSelecionado)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+    })
+})
